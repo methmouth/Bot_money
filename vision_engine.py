61d19f1856801c3551ae025d7a5795a85ddf39e9
methmouth <chalmerssupernintendo469@gmail.com>
2025-12-28 23:01:03 -0600

    Actualizar vision_engine.py

--- vision_engine.py
+++ vision_engine.py
@@ -3,28 +3,36 @@ import mss
 import numpy as np
 import pygetwindow as gw
 from ultralytics import YOLO
-from deep_sort_realtime.deepsort_tracker import DeepSort
 
 class VisionEngine:
-    def __init__(self, config):
+    def __init__(self, config, url_stream=None):
         self.model = YOLO(config['yolo_model'])
         self.config = config
-        self.sct = mss.mss()
-        self.deepsort = DeepSort(max_age=30) if config['tracker_type'] == "deepsort" else None
+        self.url_stream = url_stream
+        
+        if self.url_stream:
+            print(f"[*] Conectando a stream WiFi: {self.url_stream}")
+            self.cap = cv2.VideoCapture(self.url_stream)
+        else:
+            self.sct = mss.mss()
 
     def get_frame(self):
+        if self.url_stream:
+            ret, frame = self.cap.read()
+            return frame, None if ret else (None, None)
+        
+        # Captura original de ventana scrcpy
         try:
-            win = gw.getWindowsWithTitle(self.config['ventana_titulo'])[0]
+            windows = gw.getWindowsWithTitle(self.config['ventana_titulo'])
+            if not windows: return None, None
+            win = windows[0]
             monitor = {"top": win.top, "left": win.left, "width": win.width, "height": win.height}
             img = np.array(self.sct.grab(monitor))
             return cv2.cvtColor(img, cv2.COLOR_BGRA2BGR), win
-        except: return None, None
+        except:
+            return None, None
 
     def detect_and_track(self, frame):
-        if self.config['tracker_type'] == "bytetrack":
-            return self.model.track(frame, persist=True, tracker="bytetrack.yaml", verbose=False)[0], None
-        else:
-            results = self.model.predict(frame, conf=self.config['umbral_confianza'], verbose=False)[0]
-            detections = [([box.xyxy[0][0], box.xyxy[0][1], box.xyxy[0][2]-box.xyxy[0][0], box.xyxy[0][3]-box.xyxy[0][1]], 
-                           box.conf[0], int(box.cls[0])) for box in results.boxes]
-            return results, self.deepsort.update_tracks(detections, frame=frame)
+        # Ajuste de confianza dinámico: más estricto en WiFi por el ruido
+        conf_min = 0.55 if self.url_stream else self.config.get('umbral_confianza', 0.4)
+        return self.model.predict(frame, conf=conf_min, verbose=False)[0], None
