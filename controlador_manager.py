232b7c56ad164e7b776a42562ec7dd7f92b2c11a
methmouth <chalmerssupernintendo469@gmail.com>
2025-12-28 23:07:46 -0600

    Actualizar controlador_manager.py

--- controlador_manager.py
+++ controlador_manager.py
@@ -5,8 +5,8 @@ class ControladorHibrido:
     def __init__(self, modo="adb", ip=None):
         self.modo = modo
         self.client = None
-        if modo == "network":
-            # Conexión VNC (Asegúrate de tener un VNC Server en el móvil)
+        if modo == "network" and ip:
+            print(f"[*] Estableciendo conexión VNC con {ip}...")
             self.client = api.connect(ip, password=None) 
 
     def click(self, x_norm, y_norm, W, H):
@@ -14,7 +14,9 @@ class ControladorHibrido:
         y = int((y_norm * H) / 1000)
 
         if self.modo == "adb":
+            # [span_4](start_span)Comando original de tus archivos[span_4](end_span)
             subprocess.run(f"adb shell input tap {x} {y}", shell=True)
         else:
+            # Control vía red (WiFi)
             self.client.mouseMove(x, y)
             self.client.click(1)
